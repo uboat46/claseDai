6cb7064f6b8dc1a383ed2eade5087a8843a577b3
sdist <sdist@itam.mx>
2016-09-29 11:43:58 -0500

    conexion

---
+++ Inventario/Inventario/Conexion.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Inventario
+{
+    class Conexion
+    {
+        SqlConnection cnn;
+        SqlCommand cmd;
+        SqlDataReader dr;
+
+        public static SqlConnection Connect()
+        {
+            SqlConnection cnn;
+            cnn = new SqlConnection("Data Source=CC201-22;Initial Catalog=RegistroAlumnosBD;Persist Security Info=True;User ID=sa;Password=sqladmin");
+            cnn.Open();
+            MessageBox.Show("Conexion Exitosa");
+            return cnn;
+        }
+
+        public void llenarComboArticulos(ComboBox cb)
+        {
+            try
+            {
+                cmd = new SqlCommand("SELECT articulos.idArticulo FROM articulos", Connect());
+                dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    cb.Items.Add(dr["nombrePrograma"].ToString());
+                }
+                cb.SelectedIndex = 0;
+                dr.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        public void llenarComboUsers(ComboBox cb)
+        {
+            try
+            {
+                cmd = new SqlCommand("SELECT users.id FROM users", Connect());
+                dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    cb.Items.Add(dr["nombrePrograma"].ToString());
+                }
+                cb.SelectedIndex = 0;
+                dr.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                MessageBox.Show(e.Message);
+            }
+        }
+    }
+
+
+}
